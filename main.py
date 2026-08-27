ee3048dadb72cd3df8d3409a846bb00a21129967
Setareh-Havazadeh <setareh.havazdeh@gmail.com>
2026-08-27 11:33:52 +0330

    Add main menu

---
+++ main.py
@@ -0,0 +1,8 @@
+print("========== Expense Manager ==========")
+print("1. Add Expense")
+print("2. Show Expenses")
+print("3. Search Expenses")
+print("4. Delete Expense")
+print("5. Total Expenses")
+print("6. Statistics")
+print("7. Exit")
