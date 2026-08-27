2513c594e215dfb8aa3a93efc478156d2f5270e7
Setareh-Havazadeh <setareh.havazdeh@gmail.com>
2026-08-27 12:09:36 +0330

    Separate ExpenseManager into module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from expense_manager import ExpenseManager
+
 print("========== Expense Manager ==========")
 print("1. Add Expense")
 print("2. Show Expenses")
@@ -6,3 +8,5 @@ print("4. Delete Expense")
 print("5. Total Expenses")
 print("6. Statistics")
 print("7. Exit")
+
+value = int(input("What would you like to do? Please enter the desired number: "))
