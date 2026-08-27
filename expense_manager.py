986fc204f9a3d4ec3e7429183ff48588be5a6dec
Setareh-Havazadeh <setareh.havazdeh@gmail.com>
2026-08-27 12:52:17 +0330

    Add ExpenseManager methods

--- expense_manager.py
+++ expense_manager.py
@@ -1,3 +1,21 @@
 class ExpenseManager:
     def __init__(self):
         self.expense = []
+
+    def addExpense(self):
+        pass
+
+    def showExpenses(self):
+        pass
+
+    def searchExpenses(self):
+        pass
+
+    def deleteExpense(self):
+        pass
+
+    def totalExpenses(self):
+        pass
+
+    def statistics(self):
+        pass
